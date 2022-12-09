81f4b1616fd43b6dda02c0df98d36f49a3fa6e44
Jerre van Veluw <jerrevanveluw@users.noreply.github.com>
2022-12-09 16:03:50 +0100

    day 09 part 1

--- src/Day09.kt
+++ src/Day09.kt
@@ -1,3 +1,8 @@
+import Day09.Direction.Down
+import Day09.Direction.Left
+import Day09.Direction.Right
+import Day09.Direction.Up
+
 fun main() {
     Day09().all()
 }
@@ -5,16 +10,68 @@ fun main() {
 class Day09 : Day {
 
     override fun all() = solve {
-        part1(null)
+        part1(6087)
         part2(null)
     }
 
-    private val part1 = report { }
+    private val part1 = report {
+        val headCoordinates = mutableListOf(Coordinate(0, 0))
+        val tailCoordinates = headCoordinates.toMutableList()
+
+        forEach { headCoordinates.last().move(it, headCoordinates) }
+
+        headCoordinates
+            .zipWithNext()
+            .map { it.move(tailCoordinates) }
+
+        tailCoordinates.distinct().size
+    }
 
     private val part2 = report { }
 
-    private fun <R : Any> report(block: Sequence<String>.() -> R) = setup(block) {
-        this
+    private fun <R : Any> report(block: Sequence<Instruction>.() -> R) = setup(block) {
+        map { it.split(" ") }
+            .map { (d, s) -> Instruction(d.parse(), s.toInt()) }
+    }
+
+    enum class Direction { Up, Down, Left, Right }
+
+    private fun String.parse() = when (this) {
+        "U" -> Up
+        "D" -> Down
+        "L" -> Left
+        "R" -> Right
+        else -> throw RuntimeException()
+    }
+
+    data class Instruction(val direction: Direction, val steps: Int)
+
+    data class Coordinate(val x: Int, val y: Int) {
+        override fun toString() = "($x, ${if (y >= 0) " $y" else y})"
+    }
+
+    private fun Coordinate.closeBy(other: Coordinate): Boolean {
+        val xDiff = if (x > other.x) x - other.x else other.x - x
+        val yDiff = if (y > other.y) y - other.y else other.y - y
+        return xDiff <= 1 && yDiff <= 1
+    }
+
+    private tailrec fun Coordinate.move(instruction: Instruction, coords: MutableList<Coordinate>): List<Coordinate> =
+        if (instruction.steps == 0) coords
+        else {
+            val coord = when (instruction.direction) {
+                Up -> copy(y = y - 1)
+                Down -> copy(y = y + 1)
+                Left -> copy(x = x - 1)
+                Right -> copy(x = x + 1)
+            }
+            coords.add(coord)
+            coord.move(instruction.copy(steps = instruction.steps - 1), coords)
+        }
+
+    private fun Pair<Coordinate, Coordinate>.move(coords: MutableList<Coordinate>) {
+        val tail = coords.last()
+        coords.add(if (tail.closeBy(second)) tail else first)
     }
 
 }
--- src/Utils.kt
+++ src/Utils.kt
@@ -1,27 +1,27 @@
 import java.io.File
 import java.math.BigInteger
 import java.security.MessageDigest
+import kotlin.time.ExperimentalTime
+import kotlin.time.measureTime
 
 /**
  * Reads lines from the given input txt file.
  */
 fun readInput(name: String) = File("input", "$name.txt").readLines()
 
+@OptIn(ExperimentalTime::class)
 fun <R : Any, T : Any> Day.setup(block: T.() -> R, transform: Sequence<String>.() -> T) = { answer: R? ->
-    readInput("Day${day}").asSequence()
-        .run(transform)
-        .block()
-        .log(answer)
+    measureTime {
+        readInput("Day${day}").asSequence()
+            .run(transform)
+            .block()
+            .log(answer)
+    }.run { println("  In $inWholeMilliseconds ms") }
 }
 
-fun Any.log(correctAnswer: Any? = null) = println("  $this").also {
+fun Any.log(correctAnswer: Any? = null) = println("  Answer: $this").also {
     correctAnswer?.let { check(this == it) }
 }
-
-/**
- * Converts string to md5 hash.
- */
-fun String.md5(): String = BigInteger(1, MessageDigest.getInstance("MD5").digest(toByteArray())).toString(16)
 
 interface Day {
     val day: String get() = javaClass.simpleName.substring(3)
